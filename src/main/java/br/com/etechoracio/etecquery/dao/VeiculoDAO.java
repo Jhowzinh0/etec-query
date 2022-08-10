9719cc0a1f69f8730acc000ff938b014278a8184
Jhonatan <jhow@teste.com>
2022-08-10 15:20:35 -0300

    PWVeiculoDao

---
+++ src/main/java/br/com/etechoracio/etecquery/dao/VeiculoDAO.java
@@ -0,0 +1,17 @@
+package br.com.etechoracio.etecquery.dao;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.etechoracio.etecquery.enums.TipoCambioEnum;
+import br.com.etechoracio.etecquery.model.Veiculo;
+
+public interface VeiculoDAO extends JpaRepository<Veiculo, Long> {
+	List<Veiculo> findByTipoCambio(TipoCambioEnum tipo);
+	List<Veiculo> findByFabricante(String fabricante) ;
+	List<Veiculo> findByTipoCambioAndFabricante(TipoCambioEnum tipo, String fabricante) ;
+	List<Veiculo> findByTipoanoFabricacao( Integer anoFabricacao) ;
+	List<Veiculo> findByValorBetween(BigDecimal min, BigDecimal max) ;
+	List<Veiculo> findByTelefoneIsNotNull() ;
+}
